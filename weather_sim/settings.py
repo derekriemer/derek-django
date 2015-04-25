f227f881a4408261eb2b371c58d87dd757256718
derekriemer <derek.riemer@colorado.edu>
2015-04-25 16:16:47 -0600

    add my website to allowed_hosts

--- weather_sim/settings.py
+++ weather_sim/settings.py
@@ -24,7 +24,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'derekriemer.pythonanyware.com']
 
 
 # Application definition
