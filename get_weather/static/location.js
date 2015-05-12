44466d497379ff34127956dbdd96dbb1a89c3f0b
derekriemer <derek.riemer@colorado.edu>
2015-05-12 11:24:33 -0600

    fixed I think. The cookie wasn't for my subdomain, so I am putting this into the dom.

--- get_weather/static/location.js
+++ get_weather/static/location.js
@@ -1,28 +1,18 @@
 $(document).ready(function(){
-    console.log("we are perfect from $.ready");
     if (navigator.geolocation) {
-        console.log("location available");
         navigator.geolocation.getCurrentPosition(function(position){
-            console.log("in anonimous function of position, ", position);
-            var csrftoken = $.cookie('csrftoken');
-            console.log(csrftoken);
+            csrf_token=$("input[name='csrfmiddlewaretoken']").attr("value");
         function csrfSafeMethod(method) {
-            console.log("csrfSafeMethod");
             // these HTTP methods do not require CSRF protection
             return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
         }
         $.ajaxSetup({
             beforeSend: function(xhr, settings) {
-                console.log("in setup method");
                 if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                    console.log("in crsfsafemethod");
-                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
-                    console.log(xhr);
+                    xhr.setRequestHeader("X-CSRFToken", csrf_token);
                 }
-                console.log("exiting this function!!!!!!");
             }
         });
-        console.log("about to make a post request!!!!");
         $.post("forecast",
         {
             lat : position.coords.latitude,
@@ -30,10 +20,7 @@ $(document).ready(function(){
             page: page
         },
         function(data, status){
-            console.log("I got a reply from the page!");
-            //document.write(data);
-            console.log("I wrote the data to the page.");
-            console.log("btw, the status of the request is ", status)
+            document.write(data);
             //alert(data+"\n\n"+status);
         });
         
@@ -44,5 +31,4 @@ $(document).ready(function(){
         console.log("well, I guess no location?");
         alert("I am sorry, but I can't seem to get your location. This page requires your location to work.");
     }
-    console.log("bye");
 });
