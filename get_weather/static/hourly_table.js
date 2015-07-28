bb36e9942cd7140f80778db976e960e356154358
=derek_riemer <derek.riemer@colorado.edu>
2015-07-27 21:22:50 -0600

    Make table columns red.
    Signed-off-by: =derek_riemer <derek.riemer@colorado.edu>

--- get_weather/static/hourly_table.js
+++ get_weather/static/hourly_table.js
@@ -26,15 +26,23 @@ function makeTable(col){
 			}
 		}
 	});
-	var new_string = "<tr><th> <h2><a href =\"javascript:void(0)\" onclick=\"makeTable(0)\">Hour:</a></h2></th><th> <a href =\"javascript:void(0)\" onclick=\"makeTable(1)\">Temperature:</a></th><th> <a href =\"javascript:void(0)\" onclick=\"makeTable(2)\">Chance of precip:</a></th></tr>";
+	
+	var new_string = "<tr>";
+	new_string+="<th class='col_1'> <h2><a href =\"javascript:void(0)\" onclick=\"makeTable(0)\">Hour:</a></h2></th>";
+	new_string+="<th class='col_2'> <a href =\"javascript:void(0)\" onclick=\"makeTable(1)\">Temperature:</a></th><th class='col_3'> <a href =\"javascript:void(0)\" onclick=\"makeTable(2)\">Chance of precip:</a></th></tr>";
 	for(var e = 0; e < new_table.length; e++){
 		i = new_table[e];
-		new_string+="<tr>";
-		new_string+="<th>"+i[0][1]+"</th><td>"+i[1]+"&#176;F</td><td>"+i[2]+"%</td></tr>";
+		new_string+="<tr class='dynamic'>";
+		new_string+="<th class='col_1'>"+i[0][1]+"</th><td class='col_2'>"+i[1]+"&#176;F</td><td class='col_3'>"+i[2]+"%</td></tr>";
 	}
-	$("#dynamic_table_hourly_forecast").html(new_string)
-	new_string="Table sorted by ";
+	$("#dynamic_table_hourly_forecast").html(new_string);
+    new_string="Table sorted by ";
 	new_string+=assending[0] ? "assending " : "decending ";
 	new_string += ["hour","temperature","precipitation"][assending[1]];
 	$("#statusbar").html(new_string);
+    var colm=col+1;
+	$(".col_"+(col+1)).each(function(index, item){
+        this.setAttribute("class", "selected");
+	});
+	
 }
