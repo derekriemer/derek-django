75f6fed0d49a720227dddbc7aea78bd57634d2a2
derek_riemer <derek.riemer@colorado.edu>
2015-12-29 18:21:26 -0700

    resolved derekriemer.com/weather hopefully.

--- derek_site/urls.py
+++ derek_site/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.http import HttpResponse
 from derek_site import views
 urlpatterns = patterns('',
-    url(r'^weather/', include('get_weather.urls', namespace='weather')),
-    url(r'^personal/', include('personal.urls', namespace='personal')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^weather/?', include('get_weather.urls', namespace='weather')),
+    url(r'^personal/?', include('personal.urls', namespace='personal')),
+    url(r'^admin/?', include(admin.site.urls)),
 )
 print views
 handler404 = 'derek_site.views.error404'
